d9a6bf828af02646d572cfc31f9620030de6a1b8
George Bakogiannis <bakogiannis.george@outlook.com>
2016-01-18 00:34:30 +0200

    Started Sprites

--- include/gpu.h
+++ include/gpu.h
@@ -2,5 +2,5 @@ void gpu (int cycles);
 void gpuChangeMode(int mode);
 int gpuCheckStatus(void);
 void gpuDrawScanline(void);
-
+void gpuRenderSprites(void);
 extern unsigned char framebuffer[144][160][3];
