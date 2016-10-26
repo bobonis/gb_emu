e93203f9bd87bcc46ec82d0c11cdaf36aa3ff982
George Bakogiannis <George Bakogiannis>
2016-10-26 21:11:35 +0300

    0.91.2

--- include/sound.h
+++ include/sound.h
@@ -16,12 +16,18 @@ void end_frame(void);
 void soundWriteRegister(unsigned short address,unsigned char value);
 unsigned char soundReadRegister(unsigned short address);
 void soundReset(void);
+void soundResetControl(void);
+void soundResetRegisters(void);
+void soundResetChannel(unsigned int channel);
 void soundTurnOn(void);
 void soundTurnOff(void);
 void soundTick(void);
 void soundTickProgrammableCounter(void);
 void soundTickLenghthCounter(void);
-
+void soundTickSweepCounter(void);
+void channelEnable(unsigned int);
+void channelDisable(unsigned int);
+unsigned short channelCalculateSweepFreq(void);
 #ifdef __cplusplus
 }
 #endif
